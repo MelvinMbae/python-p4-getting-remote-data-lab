b88fe7a9ca78414125903c306febcf9385029336
MelvinMbae <melvinmuthomi95@gmail.com>
2023-11-26 17:24:18 +0300

    remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        request=requests.get(self.url)
+        return request.content
 
     def load_json(self):
-        pass
+        json_body=json.loads(self.get_response_body())
+        return json_body
